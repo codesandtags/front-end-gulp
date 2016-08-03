0ff0e61cc85a4f91854bdadec3906be7b157ea83
Edwin Torres <edwin.torres@prodigious.com>
2016-08-03 12:00:58 -0500

    Adding gitignore file and setting the gulpfile.js

--- Lesson 2/gulpfile.js
+++ Lesson 2/gulpfile.js
@@ -1,16 +1,25 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var CONFIG = {
+    "CSS": {
+        "path": "sass/**/*.scss",
+        "dest": "./css",
+        "autoprefixer": {
+            "browsers": ['last 2 versions']
+        }
+    }
+};
 
 gulp.task('default', function() {
-	console.log('hello world!');
+    console.log('hello world!');
 });
 
 gulp.task('styles', function() {
-	gulp.src('sass/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions']
-		}))
-		.pipe(gulp.dest('./css'));
+    gulp.src(CONFIG.CSS.path)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: CONFIG.CSS.autoprefixer.browsers
+        }))
+        .pipe(gulp.dest(CONFIG.CSS.dest));
 });
