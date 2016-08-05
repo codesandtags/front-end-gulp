58394796792b3f54ca2dc4f705abad4b1b4a3603
Edwin Torres <edwin.torres@prodigious.com>
2016-08-05 15:26:07 -0500

    Adding files to Lesson 3

--- Lesson 3/gulpfile.js
+++ Lesson 3/gulpfile.js
@@ -1,10 +1,22 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create(),
+    eslint = require('gulp-eslint'),
+    CONSTANTS = {
+        "js": {
+            "path": "js/**/*.js",
+            "dest": "js/"
+        },
+        "css": {
+            "path": "sass/**/*.scss",
+            "dest": "./css"
+        },
+    };
 
 gulp.task('default', ['styles'], function() {
-    gulp.watch('sass/**/*.scss', ['styles']);
+    gulp.watch(CONSTANTS.css.path, ['styles']);
+    gulp.watch(CONSTANTS.js.path, ['lint']);
 
     browserSync.init({
         server: './'
@@ -13,13 +25,18 @@ gulp.task('default', ['styles'], function() {
 });
 
 gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
+    gulp.src(CONSTANTS.css.path)
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: ['last 2 versions']
         }))
-        .pipe(gulp.dest('./css'))
+        .pipe(gulp.dest(CONSTANTS.css.dest))
         .pipe(browserSync.stream());
 });
 
-
+gulp.task('lint', function() {
+    return gulp.src([CONSTANTS.js.path])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failOnError());
+});
--- Lesson 3/js/main.js
+++ Lesson 3/js/main.js
@@ -1,5 +1,16 @@
 (function() {
+    var foo = 1,
+        cosa = 'cosa',
+        CONSTANTS = {
+            'js': {
+                'path': 'js/**/*.js',
+                'dest': 'js/'
+            },
+            'css': {
+                'path': 'sass/**/*.scss',
+                'dest': './css'
+            }
+        };
 
-	foo = 1;
-
+    return foo + CONSTANTS;
 })();
